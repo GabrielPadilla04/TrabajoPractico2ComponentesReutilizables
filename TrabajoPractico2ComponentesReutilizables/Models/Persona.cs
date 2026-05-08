9cff0a92e82c5dd13ec33a8ec1a073684f9c8c43
Gabi <gabipadillagp04@gmail.com>
2026-05-08 19:42:30 -0300

    creamos carpeta models, codificamos la clase persona.cs y home

--- TrabajoPractico2ComponentesReutilizables/Models/Persona.cs
+++ TrabajoPractico2ComponentesReutilizables/Models/Persona.cs
@@ -1,1 +1,12 @@
-﻿
+﻿namespace TrabajoPractico2ComponentesReutilizables.Models
+{
+    public class Persona
+    {
+        public int Id { get; set; }
+        public string NombreCompleto { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public string Domicilio { get; set; } = string.Empty;
+        public DateTime FechaNacimiento { get; set; }
+        public string RutaImagen { get; set; } = string.Empty;
+    }
+}
